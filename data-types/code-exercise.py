182de3bb45145d95b7d80ef47797094c412e7189
Elmi Elmi <elmielmi522@gmail.com>
2021-09-26 03:38:02 -0500

    code exercise

---
+++ data-types/code-exercise.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+num1 = two_digit_number[0]
+num2 = two_digit_number[1]
+result = int(num1) + int(num2)
+print(result)
