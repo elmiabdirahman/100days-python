bc24f2d096cfacd6b5919251825712a8022cb5fd
Elmi Elmi <elmielmi522@gmail.com>
2021-10-05 01:49:21 -0500

    input length

--- inputs.py
+++
@@ -1,7 +0,0 @@
-#print("Hello " + input("What is your name? "))
-#print(len(input("what is your name: ")))
-
-# input with variables
-name = input("what is your name: ")
-length = len(name)
-print(length)
