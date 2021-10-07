9a1853530b68a04080e45a628b7f7fcdf9829692
Elmi Elmi <elmielmi522@gmail.com>
2021-10-06 20:47:34 -0500

    nested if else

---
+++ control-flow/nested-if-else.py
@@ -0,0 +1,14 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("you can ride the rollercoaster!")
+    age = int(input("what is your age?"))
+    if age < 12:
+        print("please pay $5.")
+    elif age <= 18:
+        print("please pay $7.")
+    else:
+        print("please pay $12.")
+else:
+    print("sorry, you have to be taller before you can ride")
