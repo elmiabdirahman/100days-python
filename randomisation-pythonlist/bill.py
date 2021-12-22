6be0d766c05fb6356686e71e2a540273f49af582
Elmi Elmi <elmielmi522@gmail.com>
2021-12-22 01:43:36 -0600

    pay the bill

---
+++ randomisation-pythonlist/bill.py
@@ -0,0 +1,12 @@
+# Split string method
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+num_items = len(names)
+# Write your code below this line 👇
+
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today")
