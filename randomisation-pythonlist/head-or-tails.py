a6fd3fd11fa0d607c0209804e61a04ed98b88f43
Elmi Elmi <elmielmi522@gmail.com>
2021-12-22 01:00:01 -0600

    head or tails game

---
+++ randomisation-pythonlist/head-or-tails.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+random_side = random.randint(0, 1)
+if random_side == 0:
+    print("Head")
+else:
+    print("tails")
